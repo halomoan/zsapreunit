51d0516abafd3e2f536b758130e6fe3b9ffbcc25
Halomoan <halomoan@gmail.com>
2023-11-08 17:12:07 +0800

    Init Office

--- webapp/pages/controller/UnitsPlannerBase.controller.js
+++ webapp/pages/controller/UnitsPlannerBase.controller.js
@@ -1,19 +1,54 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/f/library",
-    'sap/ui/model/json/JSONModel'
+    "sap/ui/core/format/DateFormat",
+    "sap/ui/model/json/JSONModel"
+    
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,fioriLibrary,JSONModel) {
+    function (Controller,fioriLibrary,DateFormat,JSONModel) {
         "use strict";
 
+        
+        function getBasePath() {
+            return sap.ui.require.toUrl("sap/suite/ui/commons/sample/Timeline");
+        }
+
+        function convertData(oEvent) {
+            var oData,
+                oModel = oEvent.getSource(),
+                sBasePath = getBasePath();
+
+            if (!oEvent.getParameters().success) {
+                return;
+            }            
+
+            
+            oData = oModel.getData();
+            
+            oData.Staggered.forEach(function (oStaggered) {
+                //oStaggered.StartDate = DateUtils.parseDate(oStaggered.StartDate);
+                //oStaggered.StartDate = new Date(oStaggered.StartDate);
+                oStaggered.Photo = sBasePath + oStaggered.Photo;
+            });
+
+           
+            
+            oModel.updateBindings(true);
+        }
+
         return Controller.extend("zsapreunit.pages.controller.UnitsPlannerBase", {
+            
+
+
             onInit: function () {
                 //this.oView = this.getView();
-                this.oModel = new JSONModel(sap.ui.require.toUrl("zsapreunit/mockdata/unitplannermaster.json"));
-			    this.getView().setModel(this.oModel);
+                var oModel = new JSONModel(sap.ui.require.toUrl("zsapreunit/mockdata/unitplannermaster.json"));
+                oModel.attachRequestCompleted(convertData);
+			    this.getView().setModel(oModel);                
+
             },
             onListItemPress: function (oEvent) {
                 
@@ -29,6 +64,11 @@ sap.ui.define([
             },
             onMidClose: function(){
                 this.getView().byId("fcl").setLayout(fioriLibrary.LayoutType.StartColumnFullScreen);
+            },
+
+            formatDateTime: function(dateTime) {
+                var oDateInstance = DateFormat.getDateInstance();                
+                return oDateInstance.format(oDateInstance.parse(dateTime));
             },
             onOpen: function(){
                 // forward compact/cozy style into Dialog
