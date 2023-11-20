cf19c1b6fad2b3eb8ffb20146fbdf48695236c10
Halomoan <halomoan@gmail.com>
2023-11-20 17:59:22 +0800

    office commit

--- webapp/pages/controller/FloorUnitPlanner.controller.js
+++ webapp/pages/controller/FloorUnitPlanner.controller.js
@@ -1,9 +1,9 @@
 sap.ui.define(
-  ["zsapreunit/controller/BaseController", "sap/ui/model/json/JSONModel"],
+  ["zsapreunit/controller/BaseController", "sap/ui/model/json/JSONModel",	"sap/m/MessageToast"],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (BaseController, JSONModel) {
+  function (BaseController, JSONModel,MessageToast) {
     "use strict";
 
     return BaseController.extend(
@@ -43,7 +43,9 @@ sap.ui.define(
           this.getView().setModel(oModel);
 
           var oViewModel = new JSONModel({                    
-            "MMM-YYYY": "MMM-YYYY"                  
+            "MMM-YYYY": "MMM-YYYY",
+            "showBtn2ndTerm": false,
+            "showBtn3rdTerm": false                  
           });
           
           var oView = this.getView();
@@ -51,20 +53,59 @@ sap.ui.define(
         },
 
 
-        onShowT1Detail: function (oEvent) {
+        onShowT1Baserent: function (oEvent) {
 
           var oSource = oEvent.getSource();
           var sPath = oSource.getBindingContext().getPath();
           var oData = oSource.getBindingContext().getObject();
 
-          console.log(oData);
-          var oBaseRent = {
-              "Yr1": ""
-          };
+   
           
-          var oPopover = this.showPopOverFragment(this.getView(), oSource, this._formFragments, "zsapreunit.fragments.BaseRentYrs", this);
-          //console.log(oPopover);
+          var oPopover = this.showPopOverFragment(this.getView(), oSource, this._formFragments, "zsapreunit.fragments.BaseRentYrsChg", this);
+          
           oPopover.bindElement(sPath);
+          
+          // var oNav = sap.ui.core.Fragment.byId("container-zsapreunit---FloorUnitPlanner","baserentnv");
+          // var oChg = sap.ui.core.Fragment.byId("container-zsapreunit---FloorUnitPlanner","baserentchg");          
+          // oNav.to(oChg,"show");
+          
+
+        },
+
+        onUnitSelChange:  function(oEvent) {
+
+          
+          var oPlugin = oEvent.getSource();
+          var oTable = oEvent.getSource().getParent();
+          var oModel = this.getView().getModel("viewData");
+
+          var iIndices = oPlugin.getSelectedIndices();
+
+          
+          if (iIndices.length > 0) {          
+            var allow2ndTerm = true;
+            var allow3rdTerm = true;
+
+            for(var i=0;i<iIndices.length;i++){
+              
+              var oItem = oTable.getContextByIndex(iIndices[i]).getObject();
+             
+                if (oItem.Term2.Startdate !== "" ){
+                  allow2ndTerm = false ;                  
+                }
+
+                if (oItem.Term3.Startdate !== "" ){            
+                  
+                  allow3rdTerm = false;
+                } 
+            }     
+            oModel.setProperty("/showBtn2ndTerm",allow2ndTerm);     
+            oModel.setProperty("/showBtn3rdTerm",allow3rdTerm);     
+          } else {
+            oModel.setProperty("/showBtn2ndTerm",false);
+            oModel.setProperty("/showBtn3rdTerm",false);
+          }
+
         },
 
         onGotoFirstColumn: function(){
